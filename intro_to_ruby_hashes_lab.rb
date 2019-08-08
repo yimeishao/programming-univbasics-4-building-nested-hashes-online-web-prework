495de5e0b59b472722cf3956e5d2071a77b40182
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 03:22:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ base_hash = {:railroads => {
   :pieces => 4, 
   :rent_in_dollars => {
     :one_piece_owned => 25, 
-    :
+    :two_pieces_owned => 50, 
+    :three_pieces_owned => 100,
+    :four_pieces_owned => 200
   }
   }
   }
