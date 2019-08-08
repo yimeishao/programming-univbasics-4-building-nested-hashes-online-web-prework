417ba8cfe5c6d91ccec44f1e279e104e23a51c31
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 03:23:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,10 @@ base_hash = {:railroads => {
     :three_pieces_owned => 100,
     :four_pieces_owned => 200
   }, 
-  :names => 
+  :names => {
+    :reading_railroad => {}, 
+    
+  }
   }
   }
 end
