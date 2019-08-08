1c96f790098f4d0c316937465de1243e5690d3eb
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 03:26:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,10 +38,10 @@ base_hash = {:railroads => {
     :four_pieces_owned => 200
   }, 
   :names => {
-    :reading_railroad => {"mortgage value" => }, 
-    :pennsylvania_railroad => {},
-    :b_and_o_railroad => {},
-    :shortline_railroad => {}
+    :reading_railroad => {"mortgage_value" => "$100"}, 
+    :pennsylvania_railroad => {"mortgage_value" => "$200"},
+    :b_and_o_railroad => {"mortgage_value" => "$400"},
+    :shortline_railroad => {"mortgage_value" => "$800"}
   }
   }
   }
