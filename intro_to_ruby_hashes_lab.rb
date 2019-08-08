8b87dde918832e81f37b455981fcb8b60a21bcbe
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 03:20:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,10 @@ end
 def monopoly_with_third_tier
 base_hash = {:railroads => {
   :pieces => 4, 
-  :rent_in_dollars =>{}
+  :rent_in_dollars => {
+    :one_piece_owned => 25, 
+    :
+  }
   }
   }
 end
